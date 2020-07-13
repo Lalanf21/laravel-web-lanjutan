0862ebe97fcddd6062fbf74d4bb56c0fc5dd3768
Lalanf21 <Lalanfathurrahman@gmail.com>
2020-07-13 10:49:36 +0700

    pertemuan 11

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+    <title>Mahasiswa</title>
+</head>
+<body>
+    @include('layouts.navbar')
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="{{ asset("js/app.js") }}"></script>
+</body>
+</html>
